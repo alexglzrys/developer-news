78fea2a6a1f23a26e7015cd7b1fcb169759c31cc
Alejandro Gonzalez Reyes <jsconestilo@gmail.com>
2019-12-28 22:25:09 -0600

    Añadir archivo README

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-
-/** En Vue podemos registrar componentes de forma global o con un alcance limitado (locales). En nuestro caso crearemos la definición de un componente con alcance local que se encuentre limitado a la instancia de nuestra app vue 
- * 
+/** En Vue podemos registrar componentes de forma global o con un alcance limitado (locales). En nuestro caso crearemos la definición de un componente con alcance local que se encuentre limitado a la instancia de nuestra app vue
+ *
  * Por otra parte los componentes tambien son instancias de Vue, por lo que sus opciones son similares a las de la instancia raiz, a excepción de algunas especificas.
- * 
+ *
  * La principal razon de usar componentes es la reutilización y mantenimiento de la app. ya que internamente son modulos autonomos donde se agrupa HTML/CSS/JS, lo que permite dar mantenimiento sencillo a aplicaciones de gran escala
-*/
+ */
 const submissionComponent = {
-  props: ['submission', 'submissions'],
+  props: ["submission", "submissions"],
   // No puedo usar <template> como root element. Existe un plugin llamado Fragment para vue al estilo ReactJS <fragment>
-  template:
-    `<div style="display: flex; width: 100%">
+  template: `<div style="display: flex; width: 100%">
         <figure class="media-left">
           <img :src="submission.submissionImage" class="image is-64x64">
         </figure>
@@ -41,42 +39,43 @@ const submissionComponent = {
   // Listado de metodos de la aplicación Vue
   methods: {
     upvote(submissionId) {
-      // En este punto this.submissions es el arreglo sortedSubmissions que se le paso como valor del prop submissions a este componente. 
-      const submission = this.submissions.find(submission => submission.id === submissionId)
-      submission.votes++
+      // En este punto this.submissions es el arreglo sortedSubmissions que se le paso como valor del prop submissions a este componente.
+      const submission = this.submissions.find(
+        submission => submission.id === submissionId
+      );
+      submission.votes++;
       // Una forma más rápida y eficiente es haberle pasado el objeto completo como tal, y en esta sección incrementar el valor de la propiedad votes.. objeto.votes++
     }
-  },
-}
-
+  }
+};
 
 const footerComponent = {
-  props: ['title'],
+  props: ["title"],
   template: `<div class="footer">
     <div class="content has-text-centered">
       <strong>{{ title }}</strong> &copy;2019. Todos los derechos reservados
     </div>
   </div>`
-}
+};
 
-
-// Crear una intancia de Vue 
+// Crear una intancia de Vue
 new Vue({
-  el: '#app',
+  el: "#app",
   // Registrar los componentes a usar en el template de este componente
   // Se puede registrar tambien a manera de arreglo [submissionComponent]
   components: {
-    'submission-component': submissionComponent,
-    'footer-component': footerComponent,
+    "submission-component": submissionComponent,
+    "footer-component": footerComponent
   },
   // La instancia de Vue, puede devolver datos que deben manejarse dentro de la vista
   data() {
     return {
       // Seed el la propiedad que se inyecta al objeto window en el archivo seed.js. la cual es un objeto que contiene el path submissions que a su vez tiene como valor un arreglo de objetos
       submissions: Seed.submissions,
-      title: 'App Social Votos',
-      description: 'La red social con artículos del programador para el programador'
-    }
+      title: "Developer News",
+      description:
+        "La red social con artículos del programador para el programador"
+    };
   },
   // Las propiedades calculadas se usan para manejar calculos complejos de información que se necesitan mostrar en la vista. Y son recalculadas si alguno de sus datos (variables) dependientes cambia
   computed: {
@@ -84,8 +83,8 @@ new Vue({
       // retornar una matriz ordenada de mayor a menor numero de votos
       return this.submissions.sort((a, b) => {
         // Si el resultado es negativo, a va primero, 0 se quedan igual, positivo b va primero
-        return b.votes - a.votes
-      })
+        return b.votes - a.votes;
+      });
     }
-  },
-})
+  }
+});
--- seed.js
+++ seed.js
@@ -1,56 +1,56 @@
-// Inyectamos en el objeto window, una nueva propiedad llamada Seed. 
-// Cuando se invoca, se ejecuta de forma automática (IIFE) la función interna que nos devuelve a su vez un objeto con la propiedad submission, cuyo valor es el arreglo de datos falsos
+// Inyectamos en el objeto window, una nueva propiedad llamada Seed cuyo contenido es un arreglo de objetos falsos.
 
-// Por tanto, podemos acceder a el mediante Seed.submissions
-
-window.Seed = (function () {
-
-  // Fake data: arreglo con objetos que representan un envio en una red social
+window.Seed = (function() {
+  // Fake data: arreglo con objetos que representan un envio en una red social de programadores
   const submissions = [
     {
       id: 1,
-      title: 'Estado reactivo vs inmutable en VueJS',
-      description: 'El estado reactivo es una de las diferencias clave que hace que Vue sea único.',
-      url: '#',
+      title: "Estado reactivo vs inmutable en VueJS",
+      description:
+        "El estado reactivo es una de las diferencias clave que hace que Vue sea único.",
+      url: "#",
       votes: 13,
-      avatar: './public/images/avatars/daniel.jpg',
-      name: 'Rodrigo Valenzuela',
-      submissionImage: './public/images/submissions/image-yellow.png',
+      avatar: "./public/images/avatars/daniel.jpg",
+      name: "Rodrigo Valenzuela",
+      submissionImage: "./public/images/submissions/image-yellow.png"
     },
     {
       id: 2,
-      title: 'Single File Components de Vue',
-      description: 'Con los Single File Components de Vue podemos tener en un único fichero la lógica, la vista y el estilo para hacer más escalable nuestro desarrollo.',
-      url: '#',
+      title: "Single File Components de Vue",
+      description:
+        "Con los Single File Components de Vue podemos tener en un único fichero la lógica, la vista y el estilo para hacer más escalable nuestro desarrollo.",
+      url: "#",
       votes: 8,
-      avatar: './public/images/avatars/kristy.png',
-      name: 'Paulina Linares',
-      submissionImage: './public/images/submissions/image-rose.png',
+      avatar: "./public/images/avatars/kristy.png",
+      name: "Paulina Linares",
+      submissionImage: "./public/images/submissions/image-rose.png"
     },
     {
       id: 3,
-      title: 'El CLI de VueJS para proyectos robustos',
-      description: 'Vue CLI es un conjunto de herramientas para la creación rápida de prototipos, la fácil aplicación de andamios y la gestión eficiente de proyectos a gran escala.',
-      url: '#',
+      title: "El CLI de VueJS para proyectos robustos",
+      description:
+        "Vue CLI es un conjunto de herramientas para la creación rápida de prototipos, la fácil aplicación de andamios y la gestión eficiente de proyectos a gran escala.",
+      url: "#",
       votes: 17,
-      avatar: './public/images/avatars/veronika.jpg',
-      name: 'Alejandro González',
-      submissionImage: './public/images/submissions/image-steel.png',
+      avatar: "./public/images/avatars/veronika.jpg",
+      name: "Alejandro González",
+      submissionImage: "./public/images/submissions/image-steel.png"
     },
     {
       id: 4,
-      title: 'Async/Await en VueJS',
-      description: 'La finalidad de los operadores async y await es simplificar aun más la forma en la que trabajamos con las promesas.',
-      url: '#',
+      title: "Async/Await en VueJS",
+      description:
+        "La finalidad de los operadores async y await es simplificar aun más la forma en la que trabajamos con las promesas.",
+      url: "#",
       votes: 21,
-      avatar: './public/images/avatars/molly.png',
-      name: 'Fernanda Salgado',
-      submissionImage: './public/images/submissions/image-aqua.png',
+      avatar: "./public/images/avatars/molly.png",
+      name: "Fernanda Salgado",
+      submissionImage: "./public/images/submissions/image-aqua.png"
     }
   ];
 
-  // Retornar un objeto cuya propiedad submission contiene el arreglo de datos falsos
+  // Retornar un objeto con datos semilla
   return {
     submissions
-  }
-})()
+  };
+})();
